f64fa234c75401b67c1ac0db0260f4a931cc4b6c
Vinicius Dorta <vinicius_h.to_dorta@hotmail.com>
2024-03-29 18:58:40 -0300

    Add animação ao mover os cards

---
+++ src/utils/data.ts
@@ -0,0 +1,43 @@
+
+export const inicialItems = [
+    {
+        id: 1,
+        content: 'Item 1'
+    },
+    {
+        id: 2,
+        content: 'Item 2'
+    },
+    {
+        id: 3,
+        content: 'Item 3'
+    }
+]
+
+export const inProgressItems = [
+    {
+        id: 4,
+        content: 'Item 4'
+    },
+    {
+        id: 5,
+        content: 'Item 5'
+    },
+    {
+        id: 6,
+        content: 'Item 6'
+    }
+]
+
+export const inicialColumns = [
+    {
+        id: 1,
+        name: 'Backlog',
+        items: inicialItems
+    },
+    {
+        id: 2,
+        name: 'To do',
+        items: inProgressItems
+    }
+]
